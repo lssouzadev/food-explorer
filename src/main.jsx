fe197a8b777dcf115aaa847fe71b72193260e391
lssouzadev <leoss91@hotmail.com>
2023-02-25 17:09:00 -0300

    Visual da página SignUp finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ProductInfo } from './pages/ProductInfo'
+import { SignUp } from './pages/SignUp'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <ProductInfo />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
